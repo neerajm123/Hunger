f1f1a6027898cc43cfa726dad00c90a65dbb174a
Niraj <neerajm8888@gmail.com>
2023-07-25 13:49:04 +0530

    nav bar improved

--- app/src/main/java/com/example/hunger/Home.java
+++ app/src/main/java/com/example/hunger/Home.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
 
+import javax.net.ssl.SSLSessionBindingEvent;
+
 public class Home extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
 
     }
 }

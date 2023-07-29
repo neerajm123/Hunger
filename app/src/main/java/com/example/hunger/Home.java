bfcd4f6eeb7c97e10e2f9b8894f1aecdb2c5b388
Niraj <neerajm8888@gmail.com>
2023-07-29 14:44:02 +0530

    card popup

--- app/src/main/java/com/example/hunger/Home.java
+++ app/src/main/java/com/example/hunger/Home.java
@@ -3,22 +3,42 @@ package com.example.hunger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import javax.net.ssl.SSLSessionBindingEvent;
 
 public class Home extends AppCompatActivity {
 
 
-
+    Dialog mydialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
-
+        mydialog = new Dialog(this);
     }
+    public void popup(View v){
+        TextView close;
+        mydialog.setContentView(R.layout.userpop_up);
+        close =(TextView) mydialog.findViewById(R.id.closebtn);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mydialog.dismiss();
+            }
+        });
+        mydialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mydialog.show();
+    }
+
 }

b6f24dc9f9581b5457bfc024ee62036f832ddf21
Niraj <neerajm8888@gmail.com>
2023-08-06 12:06:20 +0530

    new nav bar

--- app/src/main/java/com/example/hunger/Home.java
+++ app/src/main/java/com/example/hunger/Home.java
@@ -1,24 +1,22 @@
 package com.example.hunger;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
 import android.app.Dialog;
 import android.graphics.Color;
-import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-
+import com.example.hunger.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import javax.net.ssl.SSLSessionBindingEvent;
 
 public class Home extends AppCompatActivity {
 
 
+    BottomNavigationView bottomNavigationView;
     Dialog mydialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +24,10 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         mydialog = new Dialog(this);
+        bottomNavigationView=findViewById(R.id.nav_bar);
+        bottomNavigationView.setBackground(null);
+
+
     }
     public void popup(View v){
         TextView close;
